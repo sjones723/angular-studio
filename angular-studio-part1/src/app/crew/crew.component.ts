d95257bc5f6ff67db66e6b42244bb1e15c0256e9
Sydney Jones <sydneyjones87@gmail.com>
2022-12-08 19:42:02 -0600

    mvc

---
+++ angular-studio-part1/src/app/crew/crew.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css']
+})
+export class CrewComponent implements OnInit {
+crew = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
